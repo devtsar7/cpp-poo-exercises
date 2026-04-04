591298574f24ff71fc2175463f29dbc9441d559d
devtsar7 <rayane.imbrator456@gmail.com>
2026-04-04 23:22:46 +0100

    exercice surcharges operateur

---
+++ surcharges operateur 1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+ class point3d {
+    private :
+    int x , y ,z ;
+    public :
+    point3d (int a , int b , int c):x(a),y(b),z(c){}
+    point3d operator+(const point3d &other)const{
+        return point3d(x + other.x , y + other.y ,  z + other.z);
+    };
+    point3d& operator+=(const point3d &other){
+        x += other.x ;
+        y += other.y ;
+        z += other.z ;
+        return *this ;
+        };
+    void afficher() const {
+        std::cout << "(" << x << "," << y << "," << z << ")" <<std::endl;
+    }
+
+ };
+ int main (){
+    point3d p1(10,15,30);
+     point3d p2(10,15,30);
+      point3d p3(10,15,30);
+ p1 += p2 += p3;  // test chaînage
+    p1.afficher();   // affiche le résultat
+    p2.afficher();   // vérifie p2 après le += p3
+    return 0 ;
+ }
